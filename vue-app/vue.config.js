d472a63cf3274b7c0295d29df01045ec9a6a5866
Jae Kim <jaekim0908@gmail.com>
2020-05-29 18:16:27 -0700

    change git workflow to use vue-app

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
     devtool: 'source-map',
   },
   devServer: {
+    host: '127.0.0.1',
+    port:8080,
+    public: 'localhost:8080',
     proxy: {
       '/api': {
         target: 'http://localhost:7071',
